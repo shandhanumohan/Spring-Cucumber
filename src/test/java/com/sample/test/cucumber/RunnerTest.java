294028b273ce616c2886508e9dfa57542a290b50
shandhanu05@gmail.com <Git4sh@n>
2019-09-28 15:05:14 +0530

    removed springboottest annotation from runner class.

--- src/test/java/com/sample/test/cucumber/RunnerTest.java
+++ src/test/java/com/sample/test/cucumber/RunnerTest.java
@@ -6,7 +6,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features",
         plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" },
